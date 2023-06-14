d063a35b1987fecdd8a55a7f3d010ab1077d51b7
Hossam Alzamly <hossam.alzamly@operaize.com>
2023-06-14 14:06:51 +0200

    Implement Updating a TODO item

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -13,9 +13,14 @@ export const handler = middy(
     const todoId = event.pathParameters.todoId
     const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
     // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
+    const userId = getUserId(event)
+    await updateTodo(todoId,updatedTodo,userId)
 
-
-    return undefined
+    return {
+      statusCode:204,
+      body:''
+    }
+  }
 )
 
 handler
